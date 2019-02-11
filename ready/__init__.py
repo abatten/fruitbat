5a4a2c0a80c6b126ff3d510a7fb9445dd060d975
abatten <adamjbatten@gmail.com>
2019-02-11 16:45:19 +1100

    Added some documentation

--- ready/__init__.py
+++ ready/__init__.py
@@ -1,5 +1,9 @@
 from . import estimation
 from .Frb import *
+
+__author__ = "Adam Batten (@abatten)"
+__version__ = "0.0.1"
+
 #from .frbz import *
 
 #__all__ = ['frbz']
--- ready/estimation.py
+++ ready/estimation.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-#__all__ = ['dm_to_redshift']
+# __all__ = ['dm_to_redshift']
 
 
 def calc_redshift(dm, dm_err=None, method='batten2019'):
     """
-    Returns the redshift of a given dispersion measure using a specified method.
+    Returns the redshift of a given dispersion measure using a
+    specified DM-z relation.
 
     Parameters
     ----------
@@ -13,11 +14,12 @@ def calc_redshift(dm, dm_err=None, method='batten2019'):
         Dispersion Measure.
 
     dm_err : float or None
-        The uncertianty in the dispersion measure. If dm_err is `None`, 
+        The uncertianty in the dispersion measure. If ``dm_err`` is `None`,
 
     method : string
-        The z-DM relation to use to calculate the redshift. 
-        Avaliable methods are: ``'batten2019'``, ``'inoue2004'``
+        The z-DM relation to use to calculate the redshift.
+        Avaliable methods are:
+        ``'batten2019'``, ``'inoue2004'``, ``'ioka2003'``
 
     Returns
     -------
@@ -25,14 +27,14 @@ def calc_redshift(dm, dm_err=None, method='batten2019'):
         Redshift
 
     z_err : float
-        The uncertianty in the redshift estimation. If dm_err is `None` then
-        `z_err` = 0.
+        The uncertianty in the redshift estimation. If dm_err is `None`
+        then ``z_err`` = 0.
     """
-   
+
     valid_methods = ['batten2019', 'inoue2004', 'ioka2003']
 
     if method not in valid_methods:
-        raise ValueError("""Method '{m}' is not a valid method. 
+        raise ValueError("""Method '{m}' is not a valid method.
             Valid methods are: {valid}""".format(m=method, valid=valid_methods))
 
     if method == 'batten2019':
@@ -54,12 +56,15 @@ def _dm_to_z_batten2019(dm, dm_err=None):
     """
     return 12.0, 4.0
 
-def _dm_to_z_inoue2004(dm, dm_err=None, cosmo={'HO': 70, 'Omega_m': 0.3, 'Omega_L': 0.7}):
+
+def _dm_to_z_inoue2004(dm, dm_err=None,
+                       cosmo={'HO': 70, 'Omega_m': 0.3, 'Omega_L': 0.7}):
     """
     Calculates a redshift from a dispersion measure using the DM-z
     relation from Inoue, S. 2004, MNRAS, 348(3), 999–1008.
     """
     return 50.0, 2.0
+
 
 def _dm_to_z_ioka2003(dm, dm_err=None):
     """
@@ -67,4 +72,3 @@ def _dm_to_z_ioka2003(dm, dm_err=None):
     relation from Ioka, K. 2003, ApJL, 598, L79
     """
     return 100.0, 5.0
-
