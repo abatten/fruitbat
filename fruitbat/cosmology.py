90469e47c9fabbe070c1626e28e4324e0b2fcc24
abatten <adamjbatten@gmail.com>
2019-02-16 23:54:00 +1100

    Added zhang2018

--- fruitbat/cosmology.py
+++ fruitbat/cosmology.py
@@ -1,7 +1,7 @@
 """
 Module for defining different cosmologies
 """
-
+from astropy import units as u
 
 def planck2018():
     """
@@ -13,12 +13,12 @@ def planck2018():
     
     planck2018 = {}
 
-    planck2018["HO"] = 67.66
+    planck2018["HO"] = 67.66 * u.km / u.s / u.Mpc
     planck2018["Omega_m"] = 0.3111
     planck2018["Omega_L"] = 0.6889
     planck2018["Omega_b"] = 0.04897
     planck2018["Omega_c"] = 0.2606
-    planck2018["sigma8"] = 0.8102
+    planck2018["sigma8"] = 0.8102 * u.Mpc
 
     return planck2018
 
@@ -33,12 +33,12 @@ def planck2015():
     
     planck2015 = {}
 
-    planck2015["HO"] = 67.74
+    planck2015["HO"] = 67.74 * u.km / u.s / u.Mpc 
     planck2015["Omega_m"] = 0.3089
     planck2015["Omega_L"] = 0.6911
     planck2015["Omega_b"] = 0.04860
     planck2015["Omega_c"] = 0.2589
-    planck2015["sigma8"] = 0.8159
+    planck2015["sigma8"] = 0.8159 * u.Mpc
 
     return planck2015
 
@@ -53,12 +53,12 @@ def planck2013():
     
     planck2013 = {}
 
-    planck2013["HO"] = 67.80
+    planck2013["HO"] = 67.80 * u.km / u.s / u.Mpc 
     planck2013["Omega_m"] = 0.3063
     planck2013["Omega_L"] = 0.692
     planck2013["Omega_b"] = 0.04816
     planck2013["Omega_c"] = 0.2582
-    planck2013["sigma8"] = 0.826
+    planck2013["sigma8"] = 0.826 * u.Mpc
 
     return planck2013
 
@@ -73,11 +73,11 @@ def wmap2013():
     
     wmap2013 = {}
 
-    wmap2013["HO"] = 69.32
+    wmap2013["HO"] = 69.32 * u.km / u.s / u.Mpc 
     wmap2013["Omega_m"] = 0.2865
     wmap2013["Omega_L"] = 0.7135
     wmap2013["Omega_b"] = 0.04628
     wmap2013["Omega_c"] = 0.2402
-    wmap2013["sigma8"] = 0.820
+    wmap2013["sigma8"] = 0.820 * u.Mpc
 
     return wmap2013
