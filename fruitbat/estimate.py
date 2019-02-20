2556d6b6ea2aeb240375a13b6910eaa9e9ad9cc4
abatten <adamjbatten@gmail.com>
2019-02-20 21:47:03 +1100

    Consolidated a bunch of redshift methods and tests

--- fruitbat/estimate.py
+++ fruitbat/estimate.py
@@ -3,6 +3,7 @@ from . import utils
 # __all__ = ['dm_to_redshift']
 from ._fruitbatstrings import (_docstr_sub, _cosmo_doc,
                                _methods_doc, _dm_units_doc)
+from .cosmology import cosmology_keys
 
 @_docstr_sub(dmunits=_dm_units_doc, methods=_methods_doc, cosmo=_cosmo_doc)
 def redshift(dm, dm_uncert=0.0, method='inoue2004', cosmology='planck2018'):
@@ -47,109 +48,140 @@ def redshift(dm, dm_uncert=0.0, method='inoue2004', cosmology='planck2018'):
 
     if method not in valid_methods:
         raise ValueError("""Method '{m}' is not a valid method.
-            Valid methods are: %(cosmos)s""")
+            Valid methods are: %(methods_doc)s""")
 
-    if method == 'batten2019':
-        z = _redshift_batten2019(dm, dm_uncert)
+    if cosmology not in cosmology_keys():
+        raise ValueError("""Cosmology '{c}' is not a valid cosmology.
+            Valid cosmologies are: %(cosmo)s""")
 
-    elif method == 'inoue2004':
-        z = _redshift_inoue2004(dm, dm_uncert, cosmology)
-
-    elif method == 'ioka2003':
-        z = _redshift_ioka2003(dm, dm_uncert)
-
-    elif method == 'zhang2018':
-        z = _redshift_zhang2018(dm, dm_uncert, cosmology)
+    z = _get_redshift_from_table(dm, method, cosmology)
 
     return z
 
 
-def _redshift_batten2019(dm, dm_uncert=0.0):
-    """
-    Calculates a redshft from a dispersion measure using the DM-z
-    relation from Batten, A. J. 2019, ....
-    """
-    return 12.0
+#def _redshift_batten2019(dm, dm_uncert=0.0):
+#    """
+#    Calculates a redshft from a dispersion measure using the DM-z
+#    relation from Batten, A. J. 2019, ....
+#    """
+#    return 12.0
+#
+#@_docstr_sub(dm_units=_dm_units_doc)
+#def _redshift_inoue2004(dm, dm_uncert=0.0, cosmology="planck2018"):
+#    """
+#    Calculates a redshift from a dispersion measure using the DM-z
+#    relation from Inoue2004_
+#
+#    Parameters
+#    ----------
+#    dm : float
+#        Units: %(dm_units)s
+#
+#    dm_uncert : float, optional
+#        Default: :0.0
+#
+#    cosmology : str, optional
+#        Default: `'planck2018'`
+#
+#    Returns
+#    -------
+#    z : float
+#        The redshift of the FRB.
+#    z_uncert : tuple of floats
+#        The uncertainty in the redshift of the FRB.
+#
+#    Notes
+#    -----
+#    Inoue2004_ presents the following dispersion measure and redshift relation.
+#   
+#    .. math ::
+#        \\mathrm{DM}(z) =0.92\\times 10^{-5} \\Omega_b h^2 c \\int_0^z
+#        \\frac{1 + z'}{H_0\\left(\\Omega_m (1 + z)^3 +
+#        \\Omega_\\Lambda\\right)^{1/2}} dz'
+#
+#
+#    .. _Inoue2004: http://adsabs.harvard.edu/abs/2004MNRAS.348..999I
+#    """
+#
+#    cosmo_dict = {
+#        "wmap2013": "inoue2004_wmap2013.npy",
+#        "planck2013": "inoue2004_planck2013.npy",
+#        "planck2015": "inoue2004_planck2015.npy", 
+#        "planck2018": "inoue2004_planck2018.npy",
+#        "eagle": "inoue2004_eagle.npy",
+#    }
+#
+#    lookup_table = utils.load_lookup_table(cosmo_dict[cosmology])
+#
+#    z = _get_redshift_from_table(lookup_table, dm, dm_uncert) 
+#    #z, dz_low, dz_high = _get_redshift_from_table(lookup_table, dm, dm_uncert) 
+#    return z#, (dz_low, dz_high)
+#
+#
+#def _redshift_ioka2003(dm, dm_uncert=0.0):
+#    """
+#    Calculates a redshift from a dispersion measure using the DM-z
+#    relation from Ioka, K. 2003, ApJL, 598, L79
+#    """
+#    cosmo_dict = {
+#        "wmap2013": "ioka2003_wmap2013.npy",
+#        "planck2013": "ioka2003_planck2013.npy",
+#        "planck2015": "ioka2003_planck2015.npy",
+#        "planck2018": "ioka2003_planck2018.npy",
+#        "eagle": "ioka2003_eagle.npy"
+#        }
+#    lookup_table = utils.load_lookup_table(cosmo_dict[cosmology])
+#    z = _get_redshift_from_table(lookup_table, dm, dm_uncert)
+#
+#    return z
+#
+#
+#def _redshift_zhang2018(dm, dm_uncert=0.0, cosmology="planck2018"):
+#    """
+#    Calculates a redshift from a dispersion measure using the DM-z
+#    relation from Zhang2018_
+#
+#
+#    .. _Zhang2018: https://ui.adsabs.harvard.edu/#abs/arXiv:1808.05277
+#    """
+#
+#    cosmo_dict = {
+#        "wmap2013": "zhang2018_wmap2013.npy",
+#        "planck2013": "zhang2018_planck2013.npy",
+#        "planck2015": "zhang2018_planck2015.npy",
+#        "planck2018": "zhang2018_planck2018.npy",
+#        "eagle": "zhang2018_eagle.npy"
+#        }
+#    lookup_table = utils.load_lookup_table(cosmo_dict[cosmology])
+#    z = _get_redshift_from_table(lookup_table, dm, dm_uncert)
+#
+#    return z
+#
 
-@_docstr_sub(dm_units=_dm_units_doc)
-def _redshift_inoue2004(dm, dm_uncert=0.0, cosmology="planck2018"):
+def _get_redshift_from_table(dm, method, cosmology):
     """
-    Calculates a redshift from a dispersion measure using the DM-z
-    relation from Inoue2004_
+    Loads a lookup table for a corresponding method and cosmology and reads
+    the value of the redshift correspoding the the DM value.
 
     Parameters
     ----------
-    dm : float
-        Units: %(dm_units)s
+    dm : int or float
 
-    dm_uncert : float, optional
-        Default: :0.0
+    method: str
 
-    cosmology : str, optional
-        Default: `'planck2018'`
+    cosmology: str
+        The cosmology keyword 
 
     Returns
     -------
-    z : float
-        The redshift of the FRB.
-    z_uncert : tuple of floats
-        The uncertainty in the redshift of the FRB.
-
-    Notes
-    -----
-    Inoue2004_ presents the following dispersion measure and redshift relation.
-   
-    .. math ::
-        \\mathrm{DM}(z) =0.92\\times 10^{-5} \\Omega_b h^2 c \\int_0^z
-        \\frac{1 + z'}{H_0\\left(\\Omega_m (1 + z)^3 +
-        \\Omega_\\Lambda\\right)^{1/2}} dz'
-
-
-    .. _Inoue2004: http://adsabs.harvard.edu/abs/2004MNRAS.348..999I
+    float
+        The redshift corresponding the the DM using the provided method and 
+        cosmology.
     """
 
-    cosmo_dict = {
-        "wmap2013": "inoue2004_wmap2013.npy",
-        "planck2015": "inoue2004_planck2015.npy",
-        "planck2018": "inoue2004_planck2018.npy", 
-        "planck2018+bao": "inoue2004_planck2018_bao.npy",
-    }
-
-    lookup_table = utils.load_lookup_table(cosmo_dict[cosmology])
-
-    z = _get_redshift_from_table(lookup_table, dm, dm_uncert) 
-    #z, dz_low, dz_high = _get_redshift_from_table(lookup_table, dm, dm_uncert) 
-    return z#, (dz_low, dz_high)
-
-
-def _redshift_ioka2003(dm, dm_uncert=0.0):
-    """
-    Calculates a redshift from a dispersion measure using the DM-z
-    relation from Ioka, K. 2003, ApJL, 598, L79
-    """
-    return 100.0
-
-
-def _redshift_zhang2018(dm, dm_uncert=0.0, cosmology="planck2018"):
-    """
-    Calculates a redshift from a dispersion measure using the DM-z
-    relation from Zhang2018_
-
-
-    .. _Zhang2018: https://ui.adsabs.harvard.edu/#abs/arXiv:1808.05277
-    """
-
-    cosmo_dict = {"planck2018": "zhang2018_planck2018.npy"}
-    lookup_table = utils.load_lookup_table(cosmo_dict[cosmology])
-    z = _get_redshift_from_table(lookup_table, dm, dm_uncert)
-
+    table_name = "_".join([method, cosmology]) 
+    table_extension = "npy"
+    filename = ".".join([table_name, table_extension])
+    lookup_table = utils.load_lookup_table(filename)
+    z = lookup_table(dm)[()]
     return z
-
-
-def _get_redshift_from_table(table, dm, dm_uncert):
-
-    z = table(dm)[()]
-    #dz_low = abs(z - table(dm - dm_uncert)[()])
-    #dz_high = abs(z - table(dm + dm_uncert)[()])
-
-    return z#, dz_low, dz_high
--- fruitbat/tests/test_fruitbat.py
+++ fruitbat/tests/test_fruitbat.py
@@ -1,63 +1,62 @@
 from fruitbat import Frb
 from astropy.coordinates import SkyCoord
 from astropy import units as u
+import pytest
 
 
-def test_methods():
-    """
-    Test that the different methods calculate the correct redshift for
-    planck2018 cosmology with a DM of 1000 pc cm**-3 
-    """
+class Test_Frb_Class(object):
 
-    methods = {"batten2019": 12.0,
-               "ioka2003" : 100.0,
-               "inoue2004": 0.9838829149238645,
-               "zhang2018": 1.1092684927893448,
-            }
-
+    # Create FRB objects for testing
     frb = Frb("Utmost1", dm=1000, dm_uncert=0.0)
-    for method in methods.keys():
-        z = frb.calc_redshift(method=method, cosmology="planck2018")
-        assert z == methods[method], "Test failed for: {}".format(method)
+    frb_raj_decj = Frb('Utmost1', dm=1000, raj="11:05:50.0", decj="-8:34:12.0")
+    frb_gl_gb = Frb('Utmost1', dm=1000, gl="30.5", gb="-60.2") 
 
+    # Test that methods returns the correct value for DM=1000 and planck2018
+    def test_methods(self):
+        methods = {
+                   "ioka2003" : 0.808948679473659,
+                   "inoue2004": 0.9838829149238645,
+                   "zhang2018": 1.1092684927893448,
+                }
+    
 
-#def test_frb_get_redshift_batten2019():
-#    f = Frb('Utmost1', 300, 1)
-#    assert f.calc_redshift(method='batten2019') == 12.0
+        for method in methods.keys():
+            z = self.frb.calc_redshift(method=method, cosmology="planck2018")
+            assert z == methods[method], "Test failed for: {}".format(method)
+   
 
+    def test_frb_calc_skycoords_raj_decj(self):
+        ra_str = "11:05:50.0"
+        dec_str = "-8:34:12.0"
+    
+        skycoords = self.frb_raj_decj.calc_skycoords()
+        test_skycoords = SkyCoord(ra_str, dec_str, frame="icrs", 
+                                   unit=(u.hourangle, u.deg))
 
-#def test_frb_get_redshift_ioka2003():
-#    f = Frb('Utmost1', 300, 1)
-#    assert f.calc_redshift(method='ioka2003') == 100.0
+        ra, dec = skycoords.ra.value, skycoords.dec.value
+        test_ra, test_dec = test_skycoords.ra.value, test_skycoords.dec.value
+        assert (ra, dec) == (test_ra, test_dec)
+    
+    
+    def test_frb_calc_skycoords_gl_gb(self):
+        gl_str = "30.5"
+        gb_str = "-60.2"
+    
+        skycoords = self.frb_gl_gb.calc_skycoords()
+        test_skycoords = SkyCoord(gl_str, gb_str, frame="galactic", unit=u.deg)
 
-
-#def test_frb_get_redshift_inoue2004():
-#    f = Frb('Utmost1', 1000, 0)
-#    assert f.calc_redshift(method='inoue2004', 
-#        cosmology='planck2018+bao') == (1.1961043734381342)
-
-
-def test_frb_calc_skycoords_raj_decj():
-    ra_str = "11:05:50.0"
-    dec_str = "-12:34:12.0"
-
-    f = Frb('Utmost1', dm=1000, raj=ra_str, decj=dec_str)
-    skycoords = f.calc_skycoords()
-    test_skycoords = SkyCoord(ra_str, dec_str, frame="icrs", 
-                               unit=(u.hourangle, u.deg))
-    ra, dec = skycoords.ra.value, skycoords.dec.value
-    test_ra, test_dec = test_skycoords.ra.value, test_skycoords.dec.value
-    assert (ra, dec) == (test_ra, test_dec)
-
-
-def test_frb_calc_skycoords_gl_gb():
-    gl_str = "30.5"
-    gb_str = "-60.2"
-
-    f = Frb('Utmost1', dm=1000, gl=gl_str, gb=gb_str)
-    skycoords = f.calc_skycoords()
-    test_skycoords = SkyCoord(gl_str, gb_str, frame="galactic", unit=u.deg)
-    gl, gb = skycoords.l.value, skycoords.b.value
-    test_gl, test_gb = test_skycoords.l.value, test_skycoords.b.value
-    assert (gl, gb) == (test_gl, test_gb)
-
+        gl, gb = skycoords.l.value, skycoords.b.value
+        test_gl, test_gb = test_skycoords.l.value, test_skycoords.b.value
+        assert (gl, gb) == (test_gl, test_gb)
+    
+    
+    def test_invalid_method(self):
+        invalid_method = "jacqui1992" 
+        with pytest.raises(ValueError):
+            self.frb.calc_redshift(invalid_method)
+            
+    
+    def test_invalid_cosmology(self):
+        invalid_cosmology = "cosmos_1964"
+        with pytest.raises(ValueError):
+            self.frb.calc_redshift(invalid_cosmology)
