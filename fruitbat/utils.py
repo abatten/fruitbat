2fb6c4b4dffd39d71fa2d244aac065f9e9d454fc
abatten <adamjbatten@gmail.com>
2019-03-12 19:21:38 +1100

    Added some better docstrings

--- fruitbat/utils.py
+++ fruitbat/utils.py
@@ -41,16 +41,18 @@ def load_lookup_table(filename, data_dir='data'):
 def create_lookup_table(filename, method, cosmo, zmin=0, zmax=20,
                         num_samples=1e4, **kwargs):
     """
-    Creates a lookup table
+    Creates an interpolated 1D redshift lookup table which can be read in 
+    using :func:`load_lookup_table`.
 
     Parameters
     ----------
     filename : str
-        The name
+        The name of the output file.
 
     method : str
+        The DM-z relation to assume when creating the table.
 
-    cosmo : dict
+    cosmo : An :obj:`astropy.cosmology` object
         A dictionary containing the cosmology parameters for the Hubble
         constant, Matter density, Baryon density and Dark Energy density.
         `cosmology` must contain values for the following keys:
@@ -84,11 +86,6 @@ def create_lookup_table(filename, method, cosmo, zmin=0, zmax=20,
 
     """
 
-#    method_table_dict = {"inoue2004": _create_lookup_table_inoue2004,
-#                         "zhang2018": _create_lookup_table_zhang2018,
-#                         "ioka2003": _create_lookup_table_ioka2003
-#                         }
-
     dm_method_dict = {
         "ioka2003": _calc_dm_ioka2003,
         "inoue2004": _calc_dm_inoue2004,
@@ -104,21 +101,23 @@ def create_lookup_table(filename, method, cosmo, zmin=0, zmax=20,
     _save_lookup_table(interp, filename)
 
 
-#    method_table_dict[method](filename, cosmology, zmin=zmin, zmax=zmax,
-#                              num_samples=num_samples)
-
-
 def _fz_integrand(z, cosmo):
     """
-    Calculate the integrand for a given redshift and cosmology. This integral
-    appears in zhang2018, inoue2004 and ioka2003.
+    Calculate the integrand for a given redshift and cosmology. This the 
+    integrand for integral that appears in zhang2018, inoue2004 and ioka2003.
 
     Parameters
     ----------
     z : float
-        The redshift to evaluate the integral
+        The input redshift.
 
-    cosmology
+    cosmo : An :obj:`astropy.cosmology` object
+        The cosmology object
+
+    Returns
+    -------
+    F(z) : float
+        The evaluated integral.
     """
 
     w = cosmo.w(z)
@@ -130,19 +129,20 @@ def _fz_integrand(z, cosmo):
 
 def _calc_dm_ioka2003(z, cosmo, zmin=0):
     """
-    Calculate the dispersion measure from a redshift given a cosmology
+    Calculate the dispersion measure of a redshift ``z`` given a cosmology
     using the Ioka (2003) relation.
 
     Parameters
     ----------
     z: float or int
-        The redshift
+        The input redshift.
+
     cosmo:
 
     Returns
     -------
     dm : float
-        The dispersion measure at the redhsift.
+        The dispersion measure at the redshift ``z``.
     """
     # Calculate Ioka 2003 DM coefficient
     coeff_top = 3 * const.c * cosmo.H0 * cosmo.Ob0
@@ -158,20 +158,23 @@ def _calc_dm_ioka2003(z, cosmo, zmin=0):
 
 def _calc_dm_inoue2004(z, cosmo, zmin=0):
     """
-    Calculate the dispersion measure from a redshift given a cosmology
+    Calculate the dispersion measure of a redshift ``z`` given a cosmology
     using the Inoue (2004) relation.
 
     Parameters
     ----------
-    z :
+    z : float
+        The input redshift.
+
     cosmology :
+
     zmin: float or int, optional
-        The minimum redshift
+    The minimum redshift for the table. Default: 0
 
     Returns
     -------
     dm : float
-        The dispersion measure at the redshift.
+        The dispersion measure at the redshift ``z``.
 
     """
     inoue_n_e_0 = 9.2e-10 * ((u.Mpc**2 * u.s**2) / (u.km**2 * u.cm**3))
@@ -226,165 +229,6 @@ def _calc_dm_zhang2018(z, cosmo, zmin=0, **kwargs):
     dm = coeff * f_igm * free_elec * integrate.quad(_fz_integrand, zmin, z,
                                                     args=(cosmo))[0]
     return dm.value
-
-#def _create_lookup_table_ioka2003(filename, cosmo, zmin=0, zmax=20,
-#                                  num_samples=1e4):
-#    """
-#    Creates an interpolated 1D DM-z look up table using the Ioka (2003)
-#    relation and a given cosmology.
-#
-#    Parameters
-#    ----------
-#    filename: str
-#
-#    cosmo: dict
-#
-#    zmin: int or float, optional
-#        The minimum redshift for the table. The output table will
-#        not be able to estimate redshifts lower than this value. Default: 0
-#
-#    zmax: int or float, optional
-#        The maximum redshift for the table. The output table will
-#        not be able to estimate redshifts higher than this value. Default: 20
-#
-#    num_samples: int, optional
-#        Default: 10000
-#
-#    Returns
-#    -------
-#    None
-#    """
-#
-#    def _calc_dm(z, cosmo):
-#        """
-#        Calculate the dispersion measure from a redshift given a cosmology
-#        using the Ioka (2003) relation.
-#        """
-#        # Check that the user has provided all the required values
-#        #cosmo_required_keys = ["HO", "Omega_m", "Omega_b", "Omega_L"]
-#        #_check_keys_in_dict(cosmo, cosmo_required_keys)
-#
-#        coeff_top = 3 * const.c * cosmo.H0 * cosmo.Ob0
-#        coeff_bot = 8 * np.pi * const.G * const.m_p
-#        coeff = coeff_top / coeff_bot
-#        coeff = coeff.to("pc cm**-3")
-#
-#        dm = coeff * integrate.quad(_fz_integrand, 0, z, args=(cosmo))[0]
-#
-#        return dm.value
-#
-#    interp = _perform_interpolation(dm_func=_calc_dm, cosmology=cosmo,
-#                                    zmin=zmin, zmax=zmax,
-#                                    num_samples=num_samples)
-#
-#    _save_lookup_table(interp, filename)
-#
-#
-#
-#
-#
-#def _create_lookup_table_inoue2004(filename, cosmo, zmin=0, zmax=20,
-#                                   num_samples=1e4):
-#    """
-#    Creates an interpolated 1D DM-z look up table using the Inoue (2004)
-#    relation and a given cosmology.
-#
-#    Parameters
-#    ----------
-#    filename: str
-#
-#    cosmo: dict
-#
-#    zmin: int or float, optional
-#        The minimum redshift for the table. The output table will
-#        not be able to estimate redshifts lower than this value. Default: 0
-#
-#    zmax: int or float, optional
-#        The maximum redshift for the table. The output table will
-#        not be able to estimate redshifts higher than this value. Default: 20
-#
-#    num_samples: int, optional
-#        Default: 10000
-#
-#    Returns
-#    -------
-#    None
-#    """
-#
-#    def _calc_dm(z, cosmo):
-#        """
-#        Calculate the dispersion measure from a redshift given a cosmology
-#        using the Inoue (2004) relation.
-#        """
-#        # Check that the user has provided all the required values
-#        #cosmo_required_keys = ["HO", "Omega_m", "Omega_b", "Omega_L"]
-#        #_check_keys_in_dict(cosmo, cosmo_required_keys)
-#
-#        inoue_n_e_0 = 9.2e-10 * ((u.Mpc**2 * u.s**2) / (u.km**2 * u.cm**3))
-#
-#        coeff = inoue_n_e_0 * const.c * cosmo.Ob0 * cosmo.H0
-#        coeff = coeff.to("pc cm**-3")
-#
-#        dm = coeff * integrate.quad(_fz_integrand, 0, z, args=(cosmo))[0]
-#
-#        return dm.value
-#
-#    interp = _perform_interpolation(dm_func=_calc_dm, cosmology=cosmo,
-#                                    zmin=zmin, zmax=zmax,
-#                                    num_samples=num_samples)
-#
-#    _save_lookup_table(interp, filename)
-#
-#
-#def _create_lookup_table_zhang2018(filename, cosmo, zmin=0, zmax=20,
-#                                   num_samples=1e4, f_igm=0.83,
-#                                   free_elec=0.875):
-#    """
-#    Creates an interpolated 1D DM-z look up table using the Zhang (2018)
-#    relation and a given cosmology.
-#
-#    Parameters
-#    ----------
-#    filename: str
-#
-#    cosmo: dict
-#
-#    zmin: int or float, optional
-#        Default: 0
-#
-#    zmax: int or float, optional
-#        Default: 30
-#
-#    num_samples: int, optional
-#        Default: 100000
-#
-#    f_igm: float, optional
-#        Default: 0.83
-#
-#    free_elec: float, optional
-#        Default: 0.875
-#    """
-#
-#    def _calc_dm(z, cosmo, approx=False, *args, **kwargs):
-#
-#        # Check that the user has provided all the required values
-#        #cosmo_required_keys = ["HO", "Omega_m", "Omega_b", "Omega_L"]y
-#        #_check_keys_in_dict(cosmo, cosmo_required_keys)
-#
-#        coeff_top = 3 * const.c * cosmo.H0 * cosmo.Ob0
-#        coeff_bot = 8 * np.pi * const.G * const.m_p
-#        coeff = coeff_top / coeff_bot
-#
-#        coeff = coeff.to("pc cm**-3")
-#        dm = coeff * f_igm * free_elec * integrate.quad(_fz_integrand, 0, z,
-#                                                        args=(cosmo))[0]
-#        return dm.value
-#
-#    interp = _perform_interpolation(dm_func=_calc_dm, cosmology=cosmo,
-#                                    zmin=zmin, zmax=zmax,
-#                                    num_samples=num_samples)
-#
-#    _save_lookup_table(interp, filename)
 
 
 def _perform_interpolation(dm_func=None, zmin=None, zmax=None, cosmo=None,
